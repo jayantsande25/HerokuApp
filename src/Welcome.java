bf13ce628b181015b117a536f6568a0fd190e0c1
jayant <jayant.sande@accenture.com>
2017-01-22 12:06:16 +0000

    first commit

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+			int  d =  (int)(Math.random()*100)+1;
+		System.out.println(" Your lucky number today :"+ d);
+				
+	
+	}
+
+}
